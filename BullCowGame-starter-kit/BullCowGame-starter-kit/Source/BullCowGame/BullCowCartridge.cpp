e2c4eba8bfbea1a216cc526ad8399276624b5b9d
Svavar Pall <spg009@gmail.com>
2020-12-04 21:38:15 +0000

    up to lecture 48

--- BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to the BullCow game");
-    PrintLine("Input something and press the enter key");
+    PrintLine(TEXT("Welcome to the BullCow game"));
+    PrintLine(TEXT("Input something and press the enter key"));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "cake";
+    FString HiddenWord = TEXT("cake");
 }
