ddcfb84c5ed71a950b23d2ec60d7a138f3fc0442
Svavar <spg009@gmail.com>
2020-12-19 07:52:18 +0000

    up to lecture 91

--- BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
+++ BuildingEscape/Intermediate/Build/Win64/UE4Editor/Inc/BuildingEscape/BuildingEscape.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeBuildingEscape_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x608A9E22,
-				0x7BE458B9,
+				0xF54E1360,
+				0xF5022432,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
